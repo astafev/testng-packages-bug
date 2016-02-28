a679279ea878dd4b77f6ef75e24a8043e0fa738e
Evgeny Astafev <astafev.evgeny@gmail.com>
2016-02-28 18:05:12 +0500

    Initial commit

---
+++ src/test/java/org/springframework/Test1.java
@@ -0,0 +1,11 @@
+package org.springframework;
+
+import org.testng.annotations.Test;
+
+//nothing to do with springframework, just spring is first complicated library came up in my mind
+public class Test1 {
+    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
